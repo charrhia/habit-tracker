1adebe129fe04fafdb6cdd9c1632ee6f16a36fa5
charrhia <charlotte.rhia.smith@gmail.com>
2018-02-03 19:20:07 -0500

    added object tests for database

---
+++ spec/features/8.user_creates_input_spec.rb
@@ -0,0 +1,16 @@
+
+require 'rails_helper'
+
+feature 'Input creation' do
+
+
+  scenario 'I want to successfully create input data' do
+
+    goal = Goal.create(name: "Work Out")
+    input = Input.create(goal_id: "1")
+
+    expect(goal.name).to eq("Work Out")
+    expect(input.goal_id).to eq("1")
+
+  end
+end
