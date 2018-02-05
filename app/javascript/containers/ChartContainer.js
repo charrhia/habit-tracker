e764fbe5654d05520f7c4bcc5fb63ad51783f87e
charrhia <charlotte.rhia.smith@gmail.com>
2018-02-05 14:02:07 -0500

    passed fetch props

--- app/javascript/containers/ChartContainer.js
+++ app/javascript/containers/ChartContainer.js
@@ -5,11 +5,48 @@ class ChartContainer extends Component {
   constructor(props) {
     super(props);
     this.state = {
+      goals: {},
+      inputs: {}
     }
   }
 
 
+// goals and inputs fetch
+componentDidMount() {
+  fetch(`/api/v1/goals.json`)
+    .then(response => {
+      if (response.ok) {
+        return response;
+      } else {
+        let errorMessage = `${response.status} (${response.statusText})`,
+        error = new Error(errorMessage);
+        throw(error);
+      }
+    })
+    .then(response => {
+      return response.json()
+    })
+    .then(body => {
+      this.setState({ goals: body })
+    })
+
+    fetch(`/api/v1/inputs.json`)
+    .then(response => {
+      return response.json()
+    })
+    .then(body => {
+      this.setState({ inputs: body })
+    })
+    .catch(error => console.error(`Error in fetch: ${error.message}`));
+}
+
+
+
   render() {
+
+let goals = this.state.goals;
+let inputs = this.state.inputs;
+
     return(
 
       <div id="charting">
@@ -23,10 +60,10 @@ class ChartContainer extends Component {
 
         </div>
 
-
-        <WeeklyReportTile />
-
-
+        <WeeklyReportTile
+          goals={goals}
+          inputs={inputs}
+         />
 
 
       </div>
