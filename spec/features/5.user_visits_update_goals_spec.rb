232677a70bc30e4a84bff2d2b82b13d03b817a5f
charrhia <charlotte.rhia.smith@gmail.com>
2018-02-07 11:50:35 -0500

    tests passing

--- spec/features/5.user_visits_update_goals_spec.rb
+++ spec/features/5.user_visits_update_goals_spec.rb
@@ -4,20 +4,18 @@ feature 'update goals page' do
 
   scenario 'i should be directed to the /goals/new page when clicking on Update Goals' do
 
-    user = FactoryBot.create(:user)
-
-    goal = Goal.create!({
-      name: "Eat Healthy", user_id: user.id
-      })
-
     visit new_user_registration_path
 
-    fill_in 'Email', with: user.email
-    fill_in 'Password', with: user.password
-    fill_in 'Password confirmation', with: user.password
+    fill_in 'Email', with: 'jane@example.com'
+    fill_in 'Password', with: 'password'
+    fill_in 'Password confirmation', with: 'password'
 
     click_button 'Sign up'
+
     click_link 'Goals'
+
+    fill_in 'Goal', with: 'Eat Healthy'
+    click_button 'Add Goal'
 
     expect(page).to have_content('Eat Healthy')
     expect(page).to have_current_path('/goals/new')
@@ -26,18 +24,21 @@ feature 'update goals page' do
 
   scenario 'I should be able to delete each goal' do
 
-    Goal.create!({
-      name: "Eat Healthy", user_id: user.id
-      })
-
     visit new_user_registration_path
 
-    fill_in 'Email', with: 'john@example.com'
+    fill_in 'Email', with: 'jane@example.com'
     fill_in 'Password', with: 'password'
     fill_in 'Password confirmation', with: 'password'
 
     click_button 'Sign up'
+
     click_link 'Goals'
+
+    fill_in 'Goal', with: 'Eat Healthy'
+    click_button 'Add Goal'
+
+    expect(page).to have_content('Eat Healthy')
+    expect(page).to have_current_path('/goals/new')
 
     click_link '[x]'
     expect(page).to have_no_content('Eat Healthy')
--- spec/models/input_spec.rb
+++ spec/models/input_spec.rb
@@ -9,8 +9,10 @@ RSpec.describe Input do
   describe "input model creation" do
     it "returns list of input" do
 
-      first_goal = Goal.create(name: "Read More")
-      first_input = Input.create(goal_id: first_goal.id)
+      user_3 = FactoryBot.create(:user)
+
+      first_goal = Goal.create(name: "Read More", user_id: user_3.id)
+      first_input = Input.create(goal_id: first_goal.id, user_id: user_3.id)
 
       inputs = Input.all
 
@@ -25,6 +27,8 @@ end
 RSpec.describe Input, type: :model do
 
   it { should belong_to :goal }
+  it { should belong_to :user }
+
 
   it { should have_valid(:goal_id).when(1) }
 
