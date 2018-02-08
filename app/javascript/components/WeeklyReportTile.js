c4b809df94d2d4f33cbbb625d4f47e9c6767479b
charrhia <charlotte.rhia.smith@gmail.com>
2018-02-08 13:58:34 -0500

    sorted API data to correctly render for reports

--- app/javascript/components/WeeklyReportTile.js
+++ app/javascript/components/WeeklyReportTile.js
@@ -17,16 +17,26 @@ class WeeklyReportTile extends Component {
     let inputs = this.props.inputs;
 
 
-    const data = [
-      {name: Object.keys(inputs)[1], 'Day 2': inputs[Object.keys(inputs)[1]]},
-      {name: Object.keys(inputs)[2], 'Day 3': inputs[Object.keys(inputs)[2]]},
-      {name: Object.keys(inputs)[3], 'Day 4': inputs[Object.keys(inputs)[3]]},
-      {name: Object.keys(inputs)[4], 'Day 5': inputs[Object.keys(inputs)[4]]},
-      {name: Object.keys(inputs)[5], 'Day 6': inputs[Object.keys(inputs)[5]]},
-      {name: Object.keys(inputs)[6], 'Day 7': inputs[Object.keys(inputs)[6]]},
-      {name: Object.keys(inputs)[7], 'Day 1': inputs[Object.keys(inputs)[7]]},
-    ];
+    // const data = [
+    //   {name: Object.keys(inputs)[1], 'Day 2': inputs[Object.keys(inputs)[1]]},
+    //   {name: Object.keys(inputs)[2], 'Day 3': inputs[Object.keys(inputs)[2]]},
+    //   {name: Object.keys(inputs)[3], 'Day 4': inputs[Object.keys(inputs)[3]]},
+    //   {name: Object.keys(inputs)[4], 'Day 5': inputs[Object.keys(inputs)[4]]},
+    //   {name: Object.keys(inputs)[5], 'Day 6': inputs[Object.keys(inputs)[5]]},
+    //   {name: Object.keys(inputs)[6], 'Day 7': inputs[Object.keys(inputs)[6]]},
+    //   {name: Object.keys(inputs)[7], 'Day 1': inputs[Object.keys(inputs)[7]]},
+    // ];
 
+
+    const data = [
+      {name: 'Page A', uv: 4000, pv: 2400, amt: 2400},
+      {name: 'Page B', uv: 3000, pv: 1398, amt: 2210},
+      {name: 'Page C', uv: 2000, pv: 9800, amt: 2290},
+      {name: 'Page D', uv: 2780, pv: 3908, amt: 2000},
+      {name: 'Page E', uv: 1890, pv: 4800, amt: 2181},
+      {name: 'Page F', uv: 2390, pv: 3800, amt: 2500},
+      {name: 'Page G', uv: 3490, pv: 4300, amt: 2100},
+];
 
     return(
 
@@ -34,7 +44,18 @@ class WeeklyReportTile extends Component {
 
         <h2>Weekly Report</h2>
 
-        <BarChart width={700} height={250} data={data}>
+        <LineChart width={600} height={300} data={data}
+            margin={{top: 5, right: 30, left: 20, bottom: 5}}>
+       <XAxis dataKey="name"/>
+       <YAxis/>
+       <CartesianGrid strokeDasharray="3 3"/>
+       <Tooltip/>
+       <Legend />
+       <Line type="monotone" dataKey="pv" stroke="#8884d8" activeDot={{r: 8}}/>
+       <Line type="monotone" dataKey="uv" stroke="#82ca9d" />
+      </LineChart>
+
+        {/* <BarChart width={700} height={250} data={data}>
             <CartesianGrid strokeDasharray="3 3" />
             <XAxis dataKey="name" />
             <YAxis />
@@ -47,7 +68,7 @@ class WeeklyReportTile extends Component {
           <Bar dataKey="Day 5" fill="#ff5050" />
           <Bar dataKey="Day 6" fill="#00ffff" />
           <Bar dataKey="Day 7" fill="#ff9900" />
-        </BarChart>
+        </BarChart> */}
 
       </div>
     )
