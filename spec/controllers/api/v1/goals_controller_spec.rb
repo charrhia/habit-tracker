8767cf2fd8719aa7f728e2aa14421a14b327b1d3
charrhia <charlotte.rhia.smith@gmail.com>
2018-02-11 17:23:09 -0500

    removed homes and added more api tests

--- spec/controllers/api/v1/goals_controller_spec.rb
+++ spec/controllers/api/v1/goals_controller_spec.rb
@@ -1,21 +1,19 @@
-# require "rails_helper"
-#
-# RSpec.describe Api::V1::GoalsController, type: :controller do
-#   let!(:first_goal) { Goal.create(name: "Work out") }
-#   let!(:second_goal) { Goal.create(name: "Eat Healthy") }
-#
-#   describe "GET#index" do
-#     it "should return a list of all the goals ordered by likes" do
-#
-#       get :index
-#       returned_json = JSON.parse(response.body)
-#
-#       expect(response.status).to eq 200
-#       expect(response.content_type).to eq("application/json")
-#
-#       expect(returned_json.length).to eq 1
-#       expect(returned_json["goals"][0]["name"]).to eq first_goal.name
-#       expect(returned_json["goals"][1]["name"]).to eq second_goal.name
-#     end
-#   end
-# end
+require 'rails_helper'
+
+RSpec.describe Api::V1::GoalsController, type: :controller do
+  user_5 = FactoryBot.create(:user)
+  let!(:first_goal) { Goal.create(name: 'Eat Healthy', user_id: user_5.id) }
+
+  describe "GET#index" do
+    it "should return a list of all the goals" do
+      get :index
+      returned_json = JSON.parse(response.body)
+
+      expect(response.status).to eq 200
+      expect(response.content_type).to eq("application/json")
+      expect(returned_json.length).to eq 1
+
+      expect(returned_json["goals"][0]["name"]).to eq first_goal.name
+    end
+  end
+end
