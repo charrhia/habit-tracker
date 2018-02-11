41692a7429e74fa7709865f5a0a46cf7818d5d99
charrhia <charlotte.rhia.smith@gmail.com>
2018-02-11 15:53:44 -0500

    refactored JS components

--- app/javascript/components/MonthlyReportTile.js
+++ app/javascript/components/MonthlyReportTile.js
@@ -1,23 +1,9 @@
 import React from 'react'
-
 import {LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend, BarChart, Bar, Radar, RadarChart, PolarGrid, PolarAngleAxis, PolarRadiusAxis} from 'recharts';
-
 
 const MonthlyReportTile = (props) => {
 
-
-    let goals = props.goals;
-    let inputs = props.inputs;
-
-    // let mappedInputs = inputs.inputs.map((input) => {
-    //   return input.goal_id
-    // })
-
-    let mappedGoals = goals.goals.map((goal) => {
-      return goal.name
-    })
-
-    let mappedData = inputs.inputs.map(input => (
+    let mappedData = props.inputs.inputs.map(input => (
       {name: input.goal_id, accomplished: 1}
     ));
 
@@ -27,26 +13,20 @@ const MonthlyReportTile = (props) => {
 
         <h2>Monthly Report</h2>
 
-
         <BarChart width={750} height={250} data={mappedData}>
           <CartesianGrid strokeDasharray="3 3" />
           <XAxis dataKey="name" />
           <YAxis />
           <Tooltip />
           <Legend />
-
           <Bar dataKey="accomplished" fill="#87AC4E" />
           {
-            goals.goals.map((goal) => {
+            props.goals.goals.map((goal) => {
               return (<Bar dataKey={`${goal.id} - ${goal.name}`} fill="#00BFFF" />)
             })
           }
-
         </BarChart>
-
-
       </div>
-    )
-  }
+    )};
 
   export default MonthlyReportTile;
--- app/javascript/components/WeeklyReportTile.js
+++ app/javascript/components/WeeklyReportTile.js
@@ -1,26 +1,11 @@
 import React from 'react'
-
 import {LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend, BarChart, Bar} from 'recharts';
-
 
 const WeeklyReportTile = (props) => {
 
-
-    let goals = props.goals;
-    let inputs = props.inputs;
-
-    // let mappedInputs = inputs.inputs.map((input) => {
-    //   return input.goal_id
-    // })
-
-    let mappedGoals = goals.goals.map((goal) => {
-      return goal.name
-    })
-
-    let mappedData = inputs.inputs.map(input => (
+    let mappedData = props.inputs.inputs.map(input => (
       {name: input.goal_id, accomplished: 1}
     ));
-
 
     return(
 
@@ -34,34 +19,14 @@ const WeeklyReportTile = (props) => {
             <YAxis />
             <Tooltip />
             <Legend />
-
             <Bar dataKey="accomplished" fill="#87AC4E" />
             {
-              goals.goals.map((goal) => {
+              props.goals.goals.map((goal) => {
                 return (<Bar dataKey={`${goal.id} - ${goal.name}`} fill="#00BFFF" />)
               })
             }
-
         </BarChart>
-
-
-        {/* <BarChart width={700} height={250} data={data}>
-            <CartesianGrid strokeDasharray="3 3" />
-            <XAxis dataKey="name" />
-            <YAxis />
-            <Tooltip />
-            <Legend />
-          <Bar dataKey='Day 1' fill="#87AC4E" />
-          <Bar dataKey="Day 2" fill="#d884c7" />
-          <Bar dataKey="Day 3" fill="#0066ff" />
-          <Bar dataKey="Day 4" fill="#cccc00" />
-          <Bar dataKey="Day 5" fill="#ff5050" />
-          <Bar dataKey="Day 6" fill="#00ffff" />
-          <Bar dataKey="Day 7" fill="#ff9900" />
-        </BarChart> */}
-
       </div>
-    )
-  }
+    )}
 
   export default WeeklyReportTile;
