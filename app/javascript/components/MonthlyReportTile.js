33e12ae774f29b068dbc4a5d27d1546fdcee6d48
charrhia <charlotte.rhia.smith@gmail.com>
2018-02-14 13:05:33 -0500

    fixed all graph colors

--- app/javascript/components/MonthlyReportTile.js
+++ app/javascript/components/MonthlyReportTile.js
@@ -3,9 +3,35 @@ import {LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend, BarChart,
 
 const MonthlyReportTile = (props) => {
 
-    let mappedMonthData = props.inputs.inputs.map(input => (
-      {name: input.goal_id, accomplished: input.goal_id}
-    ));
+
+  const COLORS = [
+    "#85C1E9",
+    "#82E0AA",
+    "#BB8FCE",
+    "#F9E79F",
+    "#F5B7B1",
+    "#CCD1D1",
+    "#F5B041",
+    "#9B59B6"
+  ]
+
+  let fill;
+  let mappedGoals = props.goals.goals.map((goal, index) => (
+    fill = `${COLORS[index % COLORS.length]}`
+  ))
+
+
+    // let mappedMonthData = props.inputs.inputs.map(input => (
+    //   {name: input.goal_id, accomplished: input.goal_id}
+    // ));
+
+
+    let mappedMonthData = props.inputs.inputs.map((input, index) => (
+      {goal: input.goal_id, accomplished: index, fill: `${mappedGoals[index]}`}
+    ))
+
+
+
 
     return(
 
@@ -15,16 +41,14 @@ const MonthlyReportTile = (props) => {
 
         <BarChart width={750} height={250} data={mappedMonthData}>
           <CartesianGrid strokeDasharray="3 3" />
-          <XAxis dataKey="name" />
+          <XAxis dataKey="goal" />
           <YAxis />
-          <Tooltip />
+          {/* <Tooltip /> */}
           <Legend />
-          <Bar dataKey="accomplished" fill="#87AC4E" />
-          {
-            props.goals.goals.map((goal) => {
-              return (<Bar dataKey={`${goal.id} - ${goal.name}`} fill="#87AC4E"  />)
-            })
-          }
+          <Bar dataKey="accomplished" fill="#ffffff" barSize={50} />
+            {props.goals.goals.map((goal, index) => {
+                return (<Bar key={index} dataKey={`${goal.name}`} fill={COLORS[index % COLORS.length]}/>)
+              })}
         </BarChart>
       </div>
     )};
