8107b247555008215c10bcffbbf52e9072c34a05
charrhia <charlotte.rhia.smith@gmail.com>
2018-02-15 21:00:55 -0500

    tests

--- spec/controllers/3.goals_controller_spec.rb
+++ spec/controllers/3.goals_controller_spec.rb
@@ -34,41 +34,45 @@ RSpec.describe GoalsController, :type => :controller do
     end
   end
 
-  # describe "#create" do
-  #   it "goal create" do
-  #
-  #     current_user = FactoryBot.create(:user)
-  #     goal_1 = Goal.create(name: "work out", user_id: current_user.id)
-  #     input_1 = Input.create(goal_id: goal_1.id, user_id: current_user.id)
-  #
-  #
-  #
-  #     sign_in(current_user)
-  #
-  #     get :create
-  #
-  #     returned_json = response.body
-  #     expect(response.status).to eq 200
-  #   end
-  # end
+  describe "#show" do
+    it "goal show" do
 
-  # describe "#update" do
-  #   it "goal update" do
-  #
-  #     current_user = FactoryBot.create(:user)
-  #     goal_1 = Goal.create(name: "work out", user_id: current_user.id)
-  #     input_1 = Input.create(goal_id: goal_1.id, user_id: current_user.id)
-  #
-  #
-  #
-  #     sign_in(current_user)
-  #
-  #     get :update
-  #
-  #     returned_json = response.body
-  #     expect(response.status).to eq 200
-  #   end
-  # end
+      current_user = FactoryBot.create(:user)
+      goal_1 = Goal.create(name: "work out", user_id: current_user.id)
+      input_1 = Input.create(goal_id: goal_1.id, user_id: current_user.id)
+
+      sign_in(current_user)
+
+      get :show, params: { id: goal_1.id }
+
+      returned_json = response.body
+      expect(response.status).to eq 200
+    end
+  end
+
+  describe "#create" do
+    it "goal create" do
+
+      current_user = FactoryBot.create(:user)
+      goal_1 = Goal.create(name: "work out", user_id: current_user.id)
+      input_1 = Input.create(goal_id: goal_1.id, user_id: current_user.id)
+
+      sign_in(current_user)
+      expect{ post(:create, {}) }.to raise_error ActionController::ParameterMissing
+    end
+  end
+
+  describe "#update" do
+    it "goal update" do
+
+      current_user = FactoryBot.create(:user)
+      goal_1 = Goal.create(name: "work out", user_id: current_user.id)
+      input_1 = Input.create(goal_id: goal_1.id, user_id: current_user.id)
+
+      sign_in(current_user)
+      expect{ post(:update, params: { id: goal_1.id }) }.to raise_error ActionController::ParameterMissing
+    end
+  end
 
   describe "#edit" do
     it "goal edit" do
